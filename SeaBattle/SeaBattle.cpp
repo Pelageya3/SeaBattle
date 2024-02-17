1d095974a9578cb20b3a10949c62543a636dbf65
Pelageya <pelageyart@yandex.ru>
2024-02-17 04:21:18 +0300

    почти готово

--- SeaBattle/SeaBattle.cpp
+++ SeaBattle/SeaBattle.cpp
@@ -51,6 +51,24 @@ void printTables(const sea_field& firstTable, const sea_field& secondTable)
 				 std::setw(6) << "" << "---------------------" << std::endl; 
 }
 
+void printTable(const sea_field& firstTable)
+{
+	std::cout << std::setw(4) << "";
+	printLetters();
+	std::cout << std::endl;
+
+	std::cout << std::setw(4) << "" << "---------------------" << std::endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << std::setw(3) << i + 1 << '|';
+		printTableString(firstTable.table[i]);
+		std::cout << " | " << std::endl;
+	}
+
+	std::cout << std::setw(4) << "" << "---------------------" << std::endl;
+}
+
 void printDelay(std::string textForDelay)
 {
 	int delayPrint = 50;
@@ -113,7 +131,7 @@ void tutorial()
 	system("cls");
 }
 
-void gameModeChoice(std::string modeQuestion, std::string modeOne, std::string modeTwo)
+int gameModeChoice(std::string modeQuestion, std::string modeOne, std::string modeTwo)
 {
 	int choiceMode = 0;
 
@@ -124,12 +142,11 @@ void gameModeChoice(std::string modeQuestion, std::string modeOne, std::string m
 		std::cout << modeOne << "\n" << modeTwo << std::endl;
 		std::cin >> choiceMode;
 
-		if (choiceMode == 1)
-			//func();
-			std::cout << "ок";
-		else if (choiceMode == 2)
-			//func2();
-			std::cout << "ок";
+		if (choiceMode == 1 || choiceMode == 2)
+		{
+			system("cls");
+			return choiceMode;
+		}
 		else
 		{
 			system("cls");
@@ -251,66 +268,261 @@ void autoShipsPlacement(sea_field& field)
 
 void manualShipsPlacement(sea_field& field)
 {
-	int i, j, num, shipSize;
-	char let;
-	
-	for (shipSize = 4; shipSize >= 1; shipSize--)
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			field.table[i][j] = '~';
+
+	for (int shipSize = 4; shipSize >= 1; shipSize--)
 		for (int shipNumber = 5 - shipSize; shipNumber > 0; shipNumber--)
 		{
-			std::cout << "Введите начальную и конечную координату (" << shipSize << "-палубник):" << std::endl;
-			checkCoordinates(let, num);
-			int startLet = let;
-			int startNum = num;
-			checkCoordinates(let, num);
-			int endLet = let;
-			int endNum = num;
+			int i, j, dir;
 
-			if (endNum - startNum == shipSize - 1 && startLet == endLet || endLet - startLet == shipSize - 1 && startNum == endNum)
-				for (i = startNum, j = startLet; i < endNum && j < endLet; i++, j++)
+			while (true)
+			{
+				system("cls");
+				printTable(field);
+				int num;
+				char let;
+				std::cout << "Введите начальную координату (" << shipSize << "-палубник):" << std::endl;
+				checkCoordinates(let, num);
+				int startLet = let;
+				int startNum = num;
+				std::cout << "Введите конечную координату (" << shipSize << "-палубник):" << std::endl;
+				checkCoordinates(let, num);
+				int endLet = let;
+				int endNum = num;
+
+				if (startLet == endLet)
+					dir = 1;
+				else
+					dir = 3;
+
+				if (endNum - startNum == shipSize - 1 && startLet == endLet || endLet - startLet == shipSize - 1 && startNum == endNum)
+				{
+					i = startNum - 1;
+					j = startLet - 'a';
 					if (isShipPlaceable(i, j, dir, field, shipSize))
 						break;
-		
+				}
+
+				std::cout << "Некорректные координаты. Введите заново: " << std::endl;
+			}
 			shipsPrint(i, j, dir, shipSize, field);
 		}
 }
 
-void gameLoop()
+sea_field hideTable(sea_field& field)
+{
+	sea_field hiddenTable{};
+
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+		{
+			if (field.table[i][j] == '#')
+				hiddenTable.table[i][j] = '~';
+			else
+				hiddenTable.table[i][j] = field.table[i][j];
+		}
+	return hiddenTable;
+}
+
+void replaceWithMiss(int i, int j, sea_field& table)
+{
+	if (i < 0 || i > 9 || j < 0 || j > 9)
+		return ;
+	for (int v = i - 1; v <= i + 1; v++)
+		for (int b = j - 1; b <= j + 1; b++)
+		{
+			if (v < 0 || v > 9 || b < 0 || b > 9)
+				continue;
+			if (table.table[v][b] == '~')
+				table.table[v][b] = '*';
+		}
+}
+
+void destroyShipIfPossible(int i, int j, sea_field& table)
+{
+	int up = i;
+	for (up = i; up >= 0; up--)
+	{
+		if (table.table[up][j] == '#')
+			return;
+		if (table.table[up][j] == '*' || table.table[up][j] == '~')
+			break;
+	}
+
+	int down = i;
+	for (down = i; down < 10; down++)
+	{
+		if (table.table[down][j] == '#')
+			return;
+		if (table.table[down][j] == '*' || table.table[down][j] == '~')
+			break;
+	}
+
+	int left = j;
+	for (left = j; left >= 0; left--)
+	{
+		if (table.table[i][left] == '#')
+			return;
+		if (table.table[i][left] == '*' || table.table[i][left] == '~')
+			break;
+	}
+
+	int right = j;
+	for (right = j; right < 10; right++)
+	{
+		if (table.table[i][right] == '#')
+			return;
+		if (table.table[i][right] == '*' || table.table[i][right] == '~')
+			break;
+	}
+
+	for (int trav_up = i; trav_up > up; trav_up--)
+		replaceWithMiss(trav_up, j, table);
+	for (int trav_down = i; trav_down < down; trav_down++)
+		replaceWithMiss(trav_down, j, table);
+	for (int trav_left = j; trav_left > left; trav_left--)
+		replaceWithMiss(i, trav_left, table);
+	for (int trav_right = j; trav_right < right; trav_right++)
+		replaceWithMiss(i, trav_right, table);
+}
+
+bool playerMove(sea_field& tableTwo)
+{
+	char col;
+	int row;
+
+	std::cout << "Введите координаты выстрела: " << std::endl;
+
+	checkCoordinates(col, row);
+
+	if (tableTwo.table[row - 1][col - 'a'] == '#')
+	{
+		tableTwo.table[row - 1][col - 'a'] = 'X';
+		destroyShipIfPossible(row - 1, col - 'a', tableTwo);
+		return true;
+	}
+	else
+	{
+		tableTwo.table[row - 1][col - 'a'] = '*';
+		return false;
+	}
+}
+
+bool aiMove(sea_field& table, int intellectMode)
+{
+	int i, j;
+
+	if (intellectMode == 1)
+	{
+		while (true)
+		{
+			i = rand() % 10;
+			j = rand() % 10;
+
+			if (table.table[i][j] != 'X' && table.table[i][j] != '*')
+				break;
+		}
+
+		if (table.table[i][j] == '#')
+		{
+			table.table[i][j] = 'X';
+			destroyShipIfPossible(i, j, table);
+			return true;
+		}
+		else
+		{
+			table.table[i][j] = '*';
+			return false;
+		}
+	}
+	
+}
+
+void printModTables(sea_field& tableOne, sea_field& tableTwo, int playerMode)
+{
+	if (playerMode == 1)
+		printTables(tableOne, hideTable(tableTwo));
+	else
+		printTables(hideTable(tableOne), hideTable(tableTwo));
+}
+
+void gameLoop(int playerMode, int placementMode, int intellectMode)
 {
 	sea_field tableOne{};
 	sea_field tableTwo{};
 
-	manualShipsPlacement(tableOne);
+	if (playerMode == 1 && placementMode == 2)
+		manualShipsPlacement(tableOne);
+	else
+		autoShipsPlacement(tableOne);
+
 	autoShipsPlacement(tableTwo);
 
-	while (!allShipsDestroyed(tableOne) || !allShipsDestroyed(tableTwo))
+	while (!allShipsDestroyed(tableOne) && !allShipsDestroyed(tableTwo))
 	{
 		system("cls");
 
-		sea_field hiddenTableTwo{};
+		printModTables(tableOne, tableTwo, playerMode);
 
-		for (int i = 0; i < 10; i++)
-			for (int j = 0; j < 10; j++)
+		if (playerMode == 1)
+		{
+			while (playerMove(tableTwo))
 			{
-				if (tableTwo.table[i][j] == '#')
-					hiddenTableTwo.table[i][j] = '~';
-				else
-					hiddenTableTwo.table[i][j] = tableTwo.table[i][j];
+				system("cls");
+				printModTables(tableOne, tableTwo, playerMode);
 			}
-
-		printTables(tableOne, tableTwo); //hidden
-		std::cout << "Введите координаты выстрела: " << std::endl;
-
-		char col;
-		int row;
-
-		//стреляет компьютер
-
-		checkCoordinates(col, row);
-
-		if (tableTwo.table[row - 1][col - 'a'] == '#')
-			tableTwo.table[row - 1][col - 'a'] = 'X';
+		}
 		else
-			tableTwo.table[row - 1][col - 'a'] = '*';
+		{
+			std::cout << "Компьютер просчитывает ход";
+			Sleep(300);
+			std::cout << '.';
+			Sleep(300);
+			std::cout << '.';
+			Sleep(300);
+			std::cout << '.';
+
+			while (aiMove(tableTwo, intellectMode))
+			{
+				system("cls");
+				printModTables(tableOne, tableTwo, playerMode);
+
+				std::cout << "Компьютер просчитывает ход";
+				Sleep(300);
+				std::cout << '.';
+				Sleep(300);
+				std::cout << '.';
+				Sleep(300);
+				std::cout << '.';
+			}
+		}
+
+		system("cls");
+		printModTables(tableOne, tableTwo, playerMode);
+
+		std::cout << "Компьютер просчитывает ход";
+		Sleep(300);
+		std::cout << '.';
+		Sleep(300);
+		std::cout << '.';
+		Sleep(300);
+		std::cout << '.';
+
+		while (aiMove(tableOne, intellectMode))
+		{
+			system("cls");
+			printModTables(tableOne, tableTwo, playerMode);
+			
+			std::cout << "Компьютер просчитывает ход";
+			Sleep(300);
+			std::cout << '.';
+			Sleep(300);
+			std::cout << '.';
+			Sleep(300);
+			std::cout << '.';
+		}
 	}
 
 	if (allShipsDestroyed(tableOne))
@@ -331,21 +543,21 @@ int main()
 	std::string playerModeHVSC = "1 - Человек против компьютера\n";
 	std::string playerModeCVSC = "2 - Компьютер против компьютера\n";
 	
-	gameModeChoice(playerModeQuestion, playerModeHVSC, playerModeCVSC);
+	int playerMode = gameModeChoice(playerModeQuestion, playerModeHVSC, playerModeCVSC);
 
 	std::string placementModeQuestion = "Выберите режим расстановки кораблей: \n";
 	std::string placementModeAuto = "1 - Автоматически\n";
 	std::string placementModeManually = "2 - Вручную\n";
 
-	gameModeChoice(placementModeQuestion, placementModeAuto, placementModeManually);
+	int placementMode = gameModeChoice(placementModeQuestion, placementModeAuto, placementModeManually);
 	
 	std::string intellectModeQuestion = "Выберите уровень интеллекта компьютера: \n";
 	std::string intellectModeRandom = "1 - Случайные выстрелы\n";
 	std::string intellectModeAI = "2 - Интеллектуальная игра\n";
 
-	gameModeChoice(intellectModeQuestion, intellectModeRandom, intellectModeAI);
+	int intellectMode = gameModeChoice(intellectModeQuestion, intellectModeRandom, intellectModeAI);
 
-	gameLoop();
+	gameLoop(playerMode, placementMode, intellectMode);
 
 	return 0;
 }
